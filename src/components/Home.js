1eb03e38cc51a923a0c52d82b0d5153863d34894
nikolapivac <npivac00@fesb.hr>
2022-08-31 18:25:04 +0200

    Created Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return(
-        <></>
+        <div className="home">
+            <div className="home_info">
+                <h1 className="home_info_name">NIKOLA PIVAC</h1>
+                <p className="home_info_job">computing engineer</p>
+                <p className="home_info_asp">aspiring web developer</p>
+                <Link to={"/about-me"}><button className="home_info_btn">get to know me</button></Link>
+            </div>
+        </div>
     )
 }
 
