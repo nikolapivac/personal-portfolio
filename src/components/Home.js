fe549b3f70f617ce848f5f73df77ce468bcd4730
nikolapivac <npivac00@fesb.hr>
2022-08-31 22:43:42 +0200

    Changed color theme

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import HomeImage from "../images/home.webp";
 
 const Home = () => {
     return(
         <div className="home">
             <div className="home_info">
-                <h1 className="home_info_name">NIKOLA PIVAC</h1>
+                <h1 className="home_info_name">Nikola Pivac</h1>
                 <p className="home_info_job">computing engineer</p>
                 <p className="home_info_asp">aspiring web developer</p>
-                <Link to={"/about-me"}><button className="home_info_btn">get to know me</button></Link>
+                <Link to={"/about-me"}><button className="home_info_btn">about me</button></Link>
+            </div>
+            <div className="home_image">
+                <img alt="home" src={HomeImage} />
             </div>
         </div>
     )
