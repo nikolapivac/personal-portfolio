3e6de6574b251c918cf1448614a99dc1f7dfec03
nikolapivac <npivac00@fesb.hr>
2022-08-31 18:24:45 +0200

    Created Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return(
-        <></>
+        <div className="header">
+            <div className="header_logo">
+                <Link to={"/"} className="logo">np</Link>
+            </div>
+            <div className="header_nav">
+                <Link to={"/about-me"} className="header_nav_item">about</Link>
+                <Link to={"/projects"} className="header_nav_item">projects</Link>
+                <Link to={"/contact"} className="header_nav_item">contact</Link>
+            </div>
+        </div>
     )
 }
 
