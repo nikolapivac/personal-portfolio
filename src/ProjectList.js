f13da41265eb39e800635e6662f67717382bca11
nikolapivac <npivac00@fesb.hr>
2022-12-29 18:29:08 +0100

    Added new projects

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -6,10 +6,30 @@ import TodoImage from "./images/Todo.png";
 import LibraryImage from "./images/library.png";
 import CalcImage from "./images/calculator.png";
 import PortfolioImage from "./images/portfolio.png";
+import NewsImage from "./images/news.png";
+import MarvelImage from "./images/marvel.png";
 
 const projects = [
     {
         "id": 1,
+        "name": "News App",
+        "stack": "HTML, CSS, JavaScript, ReactJS, News API, New York Times API",
+        "desc": "News from all over the world is displayed and sorted into categories. Users can search for articles and bookmark their favorites.",
+        "image": NewsImage,
+        "github": "https://github.com/nikolapivac/news-app",
+        "live": "https://nikolapivac.github.io/news-app/"
+    },
+    {
+        "id": 2,
+        "name": "Marvel App",
+        "stack": "HTML, CSS, JavaScript, ReactJS, Marvel API",
+        "desc": "Users can search for their favorite Marvel Comics characters and bookmark them.",
+        "image": MarvelImage,
+        "github": "https://github.com/nikolapivac/marvel-app",
+        "live": "https://nikolapivac.github.io/marvel-app/"
+    },
+    {
+        "id": 3,
         "name": "Weather App",
         "stack": "HTML, CSS, JavaScript, OpenWeather API",
         "desc": "Users can search for any location in the world and get current weather information and a 5-day forecast. The background image changes based on the weather.",
@@ -18,7 +38,7 @@ const projects = [
         "live": "https://nikolapivac.github.io/weather-app/"
     },
     {
-        "id": 2,
+        "id": 4,
         "name": "Wine Store Shopping Cart App",
         "stack": "ReactJS",
         "desc": "Users can select from a variety of wines and add them to cart. Payment is not implemented.",
@@ -27,7 +47,7 @@ const projects = [
         "live": "https://nikolapivac.github.io/shopping-cart/"
     },
     {
-        "id": 3,
+        "id": 5,
         "name": "The Office Memory Card Game",
         "stack": "ReactJS",
         "desc": "User has to click on a different character each round. The user can see their current and high score.",
@@ -36,7 +56,7 @@ const projects = [
         "live": "https://nikolapivac.github.io/memory-card/"
     },
     {
-        "id": 4,
+        "id": 6,
         "name": "CV Application",
         "stack": "ReactJS",
         "desc": "Users can enter their general info, educational and practical experiences and get a nicely formed CV as a result.",
@@ -45,7 +65,7 @@ const projects = [
         "live": "https://nikolapivac.github.io/cv-app/"
     },
     {
-        "id": 5,
+        "id": 7,
         "name": "Personal Portfolio",
         "stack": "ReactJS, Sass",
         "desc": "You're on this site right now! I built this in React to practice working with components and using Sass.",
@@ -54,7 +74,7 @@ const projects = [
         "live": "/"
     },
     {
-        "id": 6,
+        "id": 8,
         "name": "Todo List App",
         "stack": "HTML, CSS, JavaScript",
         "desc": "Simple todo list where users can add independent todos or group them in projects. Todos can be checked off the list, expanded (in order to see details) and deleted.",
@@ -63,7 +83,7 @@ const projects = [
         "live": "https://nikolapivac.github.io/todo-list/"
     },
     {
-        "id": 7,
+        "id": 9,
         "name": "Local Library",
         "stack": "Node.js, Express, Pug, MongoDB, Heroku",
         "desc": "Users can create, read, update and delete authors, books, book instances and book genres.",
@@ -72,7 +92,7 @@ const projects = [
         "live": "https://stormy-mountain-77049.herokuapp.com/catalog"
     },
     {
-        "id": 8,
+        "id": 10,
         "name": "Calculator",
         "stack": "HTML, CSS, JavaScript",
         "desc": "Performs basic operations - addition, subtraction, multiplication and division.",
